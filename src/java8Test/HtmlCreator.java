ebd185d01ff18165ab30b31fb33b2bf888127fba
karlo mendoza <karlo.mendoza@gmail.com>
2018-08-08 23:29:55 -0500

    now with adds

--- src/java8Test/HtmlCreator.java
+++ src/java8Test/HtmlCreator.java
@@ -15,17 +15,16 @@ public class HtmlCreator {
 	static StringBuilder footer;
 	static int i;
 	static int pageCount = 0;
+	static String message = "<body> <p> Pale Goddess has the most beatiful pale women out there, updated weekly, next update august 12 2018, last update august 5 2018 <ul>";
+	static String addCode = "<script async src=\"http://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js\"></script><!-- Pale Godness 1 --><ins class=\"adsbygoogle\" style=\"display:block\" data-ad-client=\"ca-pub-2062482189218377\" data-ad-slot=\"9279762258\" data-ad-format=\"auto\" data-full-width-responsive=\"true\"></ins> <script> (adsbygoogle = window.adsbygoogle || []).push({}); </script>";
+	static String addCode2 = "<script async src=\"http://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js\"></script><!-- Pale Godness 1 --><ins class=\"adsbygoogle\" style=\"display:block\" data-ad-client=\"ca-pub-2062482189218377\" data-ad-slot=\"9527282065\" data-ad-format=\"auto\" data-full-width-responsive=\"true\"></ins> <script> (adsbygoogle = window.adsbygoogle || []).push({}); </script>";
 	
 	public static void main(String args[]) throws IOException{
 		header = new StringBuilder();
 		header.append("<html><head><title>Pale Goddess</title>" +
 			"<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js\"></script>");
 		header.append(System.lineSeparator());
-		header.append("<script async src=\"http://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js\"></script>");
-		header.append(System.lineSeparator());
-		header.append("<script> (adsbygoogle = window.adsbygoogle || []).push({ google_ad_client: \"ca-pub-2062482189218377\", enable_page_level_ads: true }); </script>");
 		
-		header.append(System.lineSeparator());
 		header.append("<script src=\"jquery.unveil.js\"></script>");
 		header.append(System.lineSeparator());
 		header.append("<script>  $(function() { $(\"li img\").unveil(300); }); </script>");
@@ -35,15 +34,17 @@ public class HtmlCreator {
 		header.append("</head>");
 		header.append("<br/>");
 		header.append(System.lineSeparator());
-		header.append("<body> <ul>");
+		header.append(message);
 		header.append("<br/>");
+		header.append(addCode);
+		
 		header.append(System.lineSeparator());
 
 		closer = new StringBuilder();
 		closer.append("<br/>");
 		closer.append(System.lineSeparator());
 		closer.append("</ul> ");
-		
+		closer.append(addCode2);
 		footer = new StringBuilder();
 		footer.append("</body> </html>");
 		
@@ -79,19 +80,19 @@ public class HtmlCreator {
 		
 		String append = "";
 		if(pageCount ==2 )
-			append = "<a href= \"C:\\images\\index" + ".html\">Previous Page </a>";
+			append = "<a href= \"index" + ".html\">Previous Page </a>";
 		if(pageCount  > 2 )
-			append = "<a href= \"C:\\images\\index" + (pageCount-1) + ".html\">Previous Page </a>";
+			append = "<a href= \"index" + (pageCount-1) + ".html\">Previous Page </a>";
 		
 		if(pageCount == 1){
 			try(  PrintWriter out = new PrintWriter( "C:\\images\\index" + ".html" )  ){
 				out.println(header.toString() + content + 
-						closer.toString() + "<a href= \"C:\\images\\index" + (pageCount+1) + ".html\">next Page </a>" + footer.toString());
+						closer.toString() + "<a href= \"index" + (pageCount+1) + ".html\">next Page </a>" + footer.toString());
 			} 
 		} else {
 			try(  PrintWriter out = new PrintWriter( "C:\\images\\index" + pageCount + ".html" )  ){
 				out.println(header.toString() + content + 
-						closer.toString() + append + "<a href= \"C:\\images\\index" + (pageCount+1) + ".html\">next Page </a>" + footer.toString());
+						closer.toString() + append + "<a href= \"index" + (pageCount+1) + ".html\">next Page </a>" + footer.toString());
 			} 
 		}
 			
